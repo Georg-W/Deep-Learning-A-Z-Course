ff023413ba28e17002073f04fe883f91f5a3dd22
Georg Westner <georg.westner@googlemail.com>
2017-06-21 19:55:37 +0200

    finished mega project

---
+++ Unsupervised/Mega_Case_Study/mega_case_study.py
@@ -0,0 +1,22 @@
+# Mega Case Study - Make a Hybrid Deep Learning Model
+
+
+
+# Part 1 - Identify the Frauds with the Self-Organizing Map
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
+
+#
